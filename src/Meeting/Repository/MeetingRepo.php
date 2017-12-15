c685802ce2d1d11b344282a14ca828b950999778
Vibita <vibitashruti@gmail.com>
2017-12-15 12:26:16 +0100

    :construction: Fixing variables

--- src/Meeting/Repository/MeetingRepo.php
+++ src/Meeting/Repository/MeetingRepo.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 namespace Meeting\Repository;
 
 use Meeting\Collection\MeetingCollection;
@@ -23,25 +22,25 @@ final class MeetingRepo
       $this->pdo = $pdo;
   }
 
-  public function fetchAll() : FilmCollection
+  public function fetchAll() : MeetingCollection
   {
-      $result = $this->pdo->query('SELECT id, title FROM films');
-      $films = [];
-      while ($film = $result->fetch()) {
-          $films[] = new Film($meeting['title']);
+      $result = $this->pdo->query('SELECT id, title FROM meeting');
+      $meeting = [];
+      while ($meeting = $result->fetch()) {
+          $meetings[] = new Meeting($meeting['title']);
       }
       return new MeetingCollection(...$meeting);
   }
 
-  public function get(string $name) : Film
+  public function get(string $name) : Meeting
   {
-      $statement = $this->pdo->prepare('SELECT id, title FROM films WHERE title = :name');
+      $statement = $this->pdo->prepare('SELECT id, title FROM meetings WHERE title = :name');
       $statement->execute([':name' => $name]);
-      $film = $statement->fetch();
-      if (!$film) {
+      $meeting = $statement->fetch();
+      if (!$meeting) {
           throw new InvalidDatavaseException();
       }
-      return new Film($film['title']);
+      return new Meeting($meeting['title']);
   }
 }
 
