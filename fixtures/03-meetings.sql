e07b41b8447201fc3b68bd119fc5a0450e7d02d8
Vibita <vibitashruti@gmail.com>
2017-12-15 10:48:42 +0100

    :construction: Changes in the database

--- fixtures/03-meetings.sql
+++ fixtures/03-meetings.sql
@@ -2,16 +2,16 @@ CREATE TABLE `meetings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
   `description` varchar(50) NOT NULL,
-  `start date` int(11) NOT NULL,
-  `end date` int(11) NOT NULL,
+  `start date` date(11) NOT NULL,
+  `end date` date(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `meetings` (`title`, `description`, `start date`, `end date`)
-VALUES ('1rst meeting', 'Just like that', '25/12', '26/12');
+VALUES ('1rst meeting', 'Just like that', '2512', '2612');
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `status` varchar(50) NOT NULL,
@@ -23,7 +23,7 @@ VALUES ('Saguerre', 'Vibita' , 'Main organize');
 
 
 CREATE TABLE `organizer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `organizer_id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
@@ -33,7 +33,7 @@ INSERT INTO `lecturers` (`firstname`, `lastname`)
 VALUES ('Saguerre', 'Vibita');
 
 CREATE TABLE `attendee` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `attendee_id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
