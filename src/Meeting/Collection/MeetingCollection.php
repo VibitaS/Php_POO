95f5e6ccf4dddb79f6b4849961891d09c1ce4a85
Vibita <vibitashruti@gmail.com>
2017-12-15 12:15:28 +0100

    :sparkles: Add MeetingCollection.php in ~/Collection

---
+++ src/Meeting/Collection/MeetingCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Meeting\Collection;
+
+use Meeting\Entity\Attendee;
+use Meeting\Entity\Communities;
+use Meeting\Entity\Organizer;
+use Meeting\Entity\User;
+
+/**
+ *
+ */
+final class MeetingCollection
+{
+
+  public function __construct(argument)
+  {
+    # code...
+  }
+}
+
+
+?>
